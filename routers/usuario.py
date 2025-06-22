0f15af0a49ac71312f9b68fde8f1712c09815085
jjchsan <jjchsan@gmail.com>
2025-06-21 21:30:21 -0400

    edit cambio estado usuario, modificacion token login

--- routers/usuario.py
+++ routers/usuario.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.responses import JSONResponse
 from utils import api_logger, api_telegram
 import json
-from domain.usuario.schemas import UsuarioCreate
+from domain.usuario.schemas import UsuarioCreate,UpdateUser,ChangeEstadoUser
 
 router = APIRouter(
     prefix='/usuario',
@@ -19,3 +19,12 @@ async def get_usuarios():
 async def crear_usuario(nuevo_usuario: UsuarioCreate):
     result = service.crear_usuario(nuevo_usuario) 
     return JSONResponse(status_code=200, content=result)
+@router.post("/update")
+async def update_usuario(usuario:UpdateUser):
+    result = service.update_user(usuario)
+    return JSONResponse(status_code=200,content=result)
+
+@router.post("/changeEstado")
+async def change_estado(usuario:ChangeEstadoUser):
+    result = service.change_estado_user(usuario)
+    return JSONResponse(status_code=200,content=result)
